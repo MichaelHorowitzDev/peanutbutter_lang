92ba15d6e20f73f5674ef2057a18436dd135356e
MichaelHorowitzDev <michaelhorowitzdev@gmail.com>
2023-04-17 10:02:08 -0400

    remove comments

--- app/Parser.hs
+++ app/Parser.hs
@@ -2,9 +2,8 @@ module Parser (
  parseProgram
 ) where
 
---import qualified Lexer as T
 import Text.Megaparsec
-import Text.Megaparsec.Char --hiding (space)
+import Text.Megaparsec.Char
 import qualified Text.Megaparsec.Char.Lexer as L
 import Data.Void (Void)
 import Control.Monad
