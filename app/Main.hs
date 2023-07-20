59b09bccfdcb58b68ca78e58d2a0d76121a3ad45
Your Name <you@example.com>
2023-07-19 22:03:07 -0400

    fix error when no input file passed. cleaned up main file

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,5 @@
 module Main where
-    
+
 import System.Environment (getArgs)
 import System.IO (openFile, IOMode (ReadMode), hGetContents)
 import Interpret
@@ -11,6 +11,7 @@ import InterpretError
 import qualified Data.Set as S
 import qualified Data.List.NonEmpty as NE
 import Control.Monad.Trans.Except
+import Data.Maybe (listToMaybe)
 
 makePosState :: String -> a -> PosState a
 makePosState filename s = PosState
@@ -20,27 +21,28 @@ makePosState filename s = PosState
     , pstateTabWidth = defaultTabWidth
     , pstateLinePrefix = ""
     }
-    
+
 toParseError :: InterpretError -> PosState String -> ParseErrorBundle String InterpretError
 toParseError err state = ParseErrorBundle
-    { bundleErrors = NE.singleton (FancyError ((posOffset $ offset err)) $ S.singleton (ErrorCustom err))
+    { bundleErrors = NE.singleton (FancyError (posOffset $ offset err) $ S.singleton (ErrorCustom err))
     , bundlePosState = state
     }
 
 main :: IO ()
 main = do
     args <- getArgs
-    let filename = head args
-    file <- openFile filename ReadMode
-    let thingToDo = args !! 1
-    text <- hGetContents file
-    case thingToDo of
-        "1" -> parseTest parseProgram text
-        "2" -> case parse parseProgram filename text of
+    case listToMaybe args of
+        Just filename -> evalFile filename
+        Nothing -> putStrLn "error: no input file"
+  where
+    evalFile :: String -> IO ()
+    evalFile filename = do
+        file <- openFile filename ReadMode
+        text <- hGetContents file
+        case parse parseProgram filename text of
             Left err -> putStrLn $ errorBundlePretty err
             Right stmt -> do
                 result <- runExceptT $ execNew stmt
                 case result of
                     Right () -> return ()
                     Left err -> putStr $ errorBundlePretty $ toParseError err (makePosState filename text)
-    
